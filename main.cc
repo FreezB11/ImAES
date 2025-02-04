7c7dea8fe5fca5b51086f04924920b0e453d6901
FreezB11 <yashrajrao4@gmail.com>
2025-02-04 05:42:29 +0530

    minor tweaks

--- main.cc
+++ main.cc
@@ -8,13 +8,12 @@ int main(int argc, char *argv[]){
     uc *key, *input, *iv;
     int key_len, input_len, iv_len;
 
-    key_len = hex_decode( argv[ 2 ], &key );
-    iv_len = hex_decode( argv[ 3 ], &iv );
-    input_len = hex_decode( argv[ 4 ], &input );
+    key_len = hex_decode( "thisisasecret00", &key );
+    iv_len = hex_decode( "thisistheiv", &iv );
+    input_len = hex_decode( argv[2], &input );
      
 
     if(!strcmp(argv[1], "-e")){
-        // std::cout << ImAES::encrypt<16>(input, input_len, iv, key) << std::endl;; 
         show_hex(ImAES::encrypt<16>(input, input_len, iv, key),input_len + MAC_LENGTH);
     }else if(!strcmp(argv[1], "-d")){
         show_hex(ImAES::decrypt<16>(input, input_len, iv, key), input_len - MAC_LENGTH);
